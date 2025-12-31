e1de6cedd22c90ae7df5717d30360e58532b9b68
abhitiwari2511 <abhishek.tiwari@hmritm.ac.in>
2026-01-01 02:24:36 +0530

    init site

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-center mt-8">
+      <div className="w-20 p-3 rounded-full bg-zinc-900">
+        <div className="text-white">hello</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
